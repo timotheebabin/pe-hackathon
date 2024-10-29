b780ee51c391b93f1b2ed23a2a1068aa71891096
Lucile Jogee <lucile.jogee@etu.minesparis.psl.eu>
2024-10-29 19:09:25 +0100

    Rangement des fichiers de test

---
+++ fichiers_test/.ipynb_checkpoints/Notes-checkpoint.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# Tableau :
+#
+#  matrice de booléens : vrai si il y a un trou
+#  
+# Pièces : matrice de taille minimale dans pentominos
+#
+# vérifier si une pièce va : 
+#
+# -calculer les ss-mat d'une shape
+#
+# -vérifier si pièce peut rentrer dans cet espace - sauvergarder les places prises 
+#
+# Symétries et rotations
+#
+# Passage matrice à vecteur position
+#
+#
